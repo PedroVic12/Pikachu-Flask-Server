c2ee968cc60bc79d84a29f160f29506b386c530e
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2026-02-13 09:19:38 -0300

    Update headers.py

--- batcaverna/batcaverna-project/headers.py
+++ batcaverna/batcaverna-project/headers.py
@@ -2,7 +2,7 @@
 #  Abre um novo terminal
 # Exemplos: "brave", "code" (para VSCode), "spotify",
 APLICATIVOS_PARA_ABRIR = [
-    #"brave",
+    "brave",
    "nemo", 
 ]
 
@@ -22,13 +22,13 @@ VIDEOS_YOUTUBE_PRODUTIVIDADE = [
 # Configure seus projetos aqui.
 # Para cada projeto, informe o caminho absoluto, o comando e a porta.
 PROJETOS = [
-    # {
-    #     "nome": "App Flutter Template",
-    #     "caminho": "/home/pedrov12/Documentos/GitHub/my-flutter-getx-template-app/my_template_app/",
-    #     "comando": "flutter run",
-    #     #"porta_env": None,
-    #     #"porta": 8080 # Porta padr\00o para web
-    # },
+     {
+         "nome": "App Flutter Controle Financeiro",
+         "caminho": "/home/pedrov12/Documentos/GitHub/my-flutter-getx-template-app/my_template_app/",
+         "comando": "flutter run",
+        "porta_env": None,
+        "porta": 8080 # Porta padr\00o para web
+    },
     
     {
         "nome": "Pikachu Rest API",
@@ -38,14 +38,22 @@ PROJETOS = [
         "porta": 5555 # Porta padrão do Flask Pikachu
     },
 
-
     {
-        "nome": "DISCOVER THE UNIVERSE - ASTRO-SYSTEM",
-        "caminho": "/home/pedrov12/Documentos/GitHub/Pikachu-Flask-Server/pikachu-API/astro-system/src",
-        "comando": "python main.py",
-        "porta_env": "FLASK_APP=app.py FLASK_ENV=development FLASK_RUN_PORT=5000",
-        "porta": 5000 # Porta padrão Astro-Sytem flask
+        "nome": "Desktop Tauri PLC ONS",
+        "caminho": "/home/pedrov12/Documentos/GitHub/dashboard-website-template/dashboard-must-tauri-app",
+        "comando": "cargo tauri dev",
+        "porta_env": None,
+        "porta": 5556 
     }
+
+
+    # {
+    #     "nome": "DISCOVER THE UNIVERSE - ASTRO-SYSTEM",
+    #     "caminho": "/home/pedrov12/Documentos/GitHub/Pikachu-Flask-Server/pikachu-API/astro-system/src",
+    #     "comando": "python main.py",
+    #     "porta_env": "FLASK_APP=app.py FLASK_ENV=development FLASK_RUN_PORT=5000",
+    #     "porta": 5000 # Porta padrão Astro-Sytem flask
+    # }
 ]
 
 
